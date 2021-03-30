d3f86110a319265f82c3d19c9f81fad17eb2da7a
MARKILoff <zuxriddinovsirojidin@gmail.com>
2021-03-31 01:13:00 +0300

    first commit

--- src/Layout/Footer/FooterEnd.jsx
+++ src/Layout/Footer/FooterEnd.jsx
@@ -9,18 +9,18 @@ import { Link } from 'react-router-dom';
 const FooterEnd = () => {
     return (
         <>
-        <div className="grid grid-cols-3 p-5">
-            <div></div>
-            <div className="flex justify-between">
-            <a href="https://facebookIcon.com" target="_blank"><FacebookIcon className="text-blue-900 cursor-pointer" /></a> 
-            <a href="https://twitterIcon.com" target="_blank"><TwitterIcon className="text-blue-600 cursor-pointer" /></a> 
-            <a href="https://pinterestIcon.com"><PinterestIcon className="text-green-900 cursor-pointer" /></a> 
-            <a href="https://youTubeIcon"><YouTubeIcon className="text-red-700 cursor-pointer" /></a> 
-            <a href="https://InstagramIcon"><InstagramIcon className="text-pink-500 cursor-pointer" /></a> 
+            <div className="grid grid-cols-3 p-5">
+                <div></div>
+                <div className="flex justify-between">
+                    <a href="https://facebookIcon.com" target="_blank"><FacebookIcon className="text-blue-900 cursor-pointer" /></a>
+                    <a href="https://twitterIcon.com" target="_blank"><TwitterIcon className="text-blue-600 cursor-pointer" /></a>
+                    <a href="https://pinterestIcon.com" target="_blank"><PinterestIcon className="text-green-900 cursor-pointer" /></a>
+                    <a href="https://youTubeIcon" target="_blank"><YouTubeIcon className="text-red-700 cursor-pointer" /></a>
+                    <a href="https://InstagramIcon" target="_blank"><InstagramIcon className="text-pink-500 cursor-pointer" /></a>
+                </div>
+                <div></div>
             </div>
-            <div></div>
-        </div>
-        <hr/>
+            <hr />
         </>
     )
 };
